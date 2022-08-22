14679c44c7e90cecfa2bf7caca5af2dc0bb083e7
Federico Lopez <federico.lopez@mtel.local>
2022-08-22 17:36:25 -0400

    auth terminado

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,7 +12,6 @@ const router = Router();
 
 router.post( '/',
     [
-        check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password es obligatorio').not().isEmpty(),
         validarCampos
     ],
